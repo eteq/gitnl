1bcf3b4748f680e0d1a734ab9029dc10281ac1b4
Erik Tollerud <erik.tollerud@gmail.com>
2016-06-22 11:31:05 +0100

    fix gitnl.py

--- gitnl.py
+++ gitnl.py
@@ -7,17 +7,19 @@ def parse_to_git(args):
     returns a string with what should be passed to git.  E.g.,
     "push remotename branch"
     """
-    #raise NotImplementedError
+    raise NotImplementedError
     return 'push eteq master'
 
 if __name__ == '__main__':
     allargs = ' '.join(sys.argv[1:])
-    gitcmds = readcmdline(allargs)
+    gitcmds = parse_to_git(allargs)
 
     res = input('Parsed command into "git {}". Enter to continue or anything '
                 'else to quit.'.format(gitcmds))
     if res.strip() != '':
         sys.exit(1)
 
-    gitpath = subprocess.check_output(['which', 'git'])
-    os.system([gpath] + ' '.join(gitcmds))
+    gitpath = subprocess.check_output(['which', 'git']).decode()
+    command = gitpath.strip() + ' ' + gitcmds.strip()
+    print('Doing this command: "{}"'.format(command))
+    sys.exit(os.system(command))
