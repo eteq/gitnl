c26aa262ca62130b563b4a6bc273f9961d42d9ea
Brett Andrews <brett.h.andrews@gmail.com>
2016-06-22 20:31:15 +0100

    correctly prints git commands

--- gitnl.py
+++ gitnl.py
@@ -2,8 +2,9 @@ from __future__ import print_function, division, absolute_import
 
 import os
 import sys
-from subprocess import Popen, PIPE, STDOUT, check_output
+import numpy as np
 import pandas as pd
+from subprocess import Popen, PIPE, STDOUT, check_output
 
 
 def parse_to_git(args):
@@ -11,21 +12,25 @@ def parse_to_git(args):
     returns a string with what should be passed to git.  E.g.,
     "push remotename branchname"
     """
-    print(args)
-    inds_prep = np.where(hier['pos'] in ['PRT'])[0]
-    if len(inds_prep) == 0:
+    ind_prep = np.atleast_1d(np.array(args[args['pos'] == 'PRT']['level']))
+    if len(ind_prep) == 0:
         print('No preposition found')
-    elif len(inds_prep) > 1:
+    elif len(ind_prep) > 1:
         print('Multiple prepositions found')
     action = args[args['group'] == 'ROOT']['word'].ix[0]
-    others = args[args['NOUN'] == 'NOUN']]
-    others2 = others[others['parent'] < inds_prep[0]]
-        
-    kwargs = dict(action=action)
-    cmd = 'git {action}'.format(**kwargs)
-    # raise NotImplementedError
-    # return 'push eteq master'
-    
+    others = args[args['pos'] == 'NOUN']
+    others1 = others[others['parent'] < ind_prep[0]]
+    others2 = others[others['parent'] > ind_prep[0]]
+
+    kwargs = dict(action=action, args1=others1['word'].tolist(), args2=others2['word'].tolist())
+    cmds = []
+    for a2 in kwargs['args2']:
+        for a1 in kwargs['args1']:
+            cmds.append('{0} {1} {2}'.format(action, a1, a2))
+    print('kwargs', kwargs)
+    return cmds
+
+
 
 demo_file = '/Users/andrews/software/python/models/syntaxnet/syntaxnet/demo.sh'
 phrase = 'push branch test_branch to remote github_repo.'
@@ -38,15 +43,15 @@ for i, it in enumerate(out_split):
         ind = i
         break
 
-colln_tabbed = out_split[ind:-3]  # need better way to cut off trailing useless rows 
+colln_tabbed = out_split[ind:-3]  # need better way to cut off trailing useless rows
 colln = [it.split('\t') for it in colln_tabbed]
-columns = ['level', 'word', 'ig1','pos', 'fine', 'ig2', 'parent', 'group', 'ig3', 'ig4']
+columns = ['level', 'word', 'ig1', 'pos', 'fine', 'ig2', 'parent', 'group', 'ig3', 'ig4']
 
 raw = pd.DataFrame(colln, columns=columns)
 useful_cols = ['level', 'word', 'pos', 'fine', 'parent', 'group']
 hier = raw[useful_cols]
-hier = pd.to_numeric(hier['level'])
-hier = pd.to_numeric(hier['parent'])
+hier['level'] = pd.to_numeric(hier['level'])
+hier['parent'] = pd.to_numeric(hier['parent'])
 
 
 #  level         word   pos fine parent  group
@@ -68,12 +73,19 @@ if __name__ == '__main__':
     # allargs = ' '.join(sys.argv[1:])
     gitcmds = parse_to_git(hier)
 
-    res = input('Parsed command into "git {}". Enter to continue or anything '
-                'else to quit.'.format(gitcmds))
-    if res.strip() != '':
+    print(gitcmds)
+
+    prompt = 'Options:\n'
+    for i, cmd in enumerate(gitcmds):
+        prompt += '( {0} ) git {1}\n'.format(i, cmd)
+
+    prompt += 'or type "q" to quit.\n\n'
+
+    res = input(prompt)
+    if res in ['q', 'Q', 'quit', 'Quit']:
         sys.exit(1)
 
-    gitpath = check_output(['which', 'git']).decode()
-    command = gitpath.strip() + ' ' + gitcmds.strip()
-    print('Doing this command: "{}"'.format(command))
-    sys.exit(os.system(command))
+    # gitpath = check_output(['which', 'git']).decode()
+    # command = gitpath.strip() + ' ' + gitcmds.strip()
+    print('Doing this command: "git {}"'.format(gitcmds[res]))
+    # sys.exit(os.system(command))
